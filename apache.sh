4e8718f7ccec2001086218383bf812c4bda33241
fwsGonzo <fwsgonzo@hotmail.com>
2017-11-22 16:25:45 +0100

    Fix apache2 install script

--- apache.sh
+++ apache.sh
@@ -2,6 +2,6 @@
 sudo apt install apache2
 sudo a2enmod ssl
 echo "Use your url for the Common Name (CN)"
-openssl req $@ -new -x509 -days 365 -nodes -out /etc/apache2/apache.pem -keyout /etc/apache2/apache.pe
+sudo openssl req -new -x509 -days 365 -nodes -out /etc/apache2/ssl/apache.pem -keyout /etc/apache2/ssl/apache.pem
 echo "-->"
 echo "https://debian-administration.org/article/349/Setting_up_an_SSL_server_with_Apache2"
