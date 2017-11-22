3408caf40ed320397c8fa61cbb605c30e65f7696
fwsGonzo <fwsgonzo@hotmail.com>
2017-11-22 15:45:10 +0100

    Fix gitmodules and build Botan

---
+++ build_botan.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git submodule update --init --recursive
+pushd botan
+./configure.py --amalgamation --prefix=/usr/local/botan --cc=clang --cc-abi-flags="-std=c++14 -stdlib=libc++ -march=native -fuse-ld=lld-5.0 -flto=thin -O2"
+make -j
+popd
